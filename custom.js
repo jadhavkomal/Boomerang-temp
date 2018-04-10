ac0331ef6b76c4c71d3b5c2d36c2a282a96ed001
Komal Jadhav <komaljadhav613@gmail.com>
2018-04-10 15:39:24 +0530

    replace menu-bar

--- custom.js
+++ custom.js
@@ -6,22 +6,35 @@
         $('.opaque-navbar').removeClass('opaque');
     }
 });
+
+
+/*
 function openNav() {
     document.getElementById("mySidenav").style.width = "400px";
     document.getElementById("main").style.marginLeft = "0";
     /*setTimeout(function(){
       mySidenav .classList.add("opened");
-    }, 6000);*/
+    }, 6000);
 }
+*/
 
-function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-    document.getElementById("main").style.marginLeft= "0";
+//function closeNav() {
+    //document.getElementById("mySidenav").style.right = "0";
+    //document.getElementById("main").style.marginLeft= "0";
   /* setTimeout(function(){
        mySidenav . classList.remove("opened");
     }, 600);*/
-}
+//}
 
 
 
-  
+
+jQuery('#main').click(function () {
+            jQuery('.sidenav').addClass('active-nav-menu');
+        });
+        
+        jQuery('.closebtn').click(function () {
+           jQuery('.sidenav').removeClass('active-nav-menu');
+        });
+        
+
